ed3cb9175c477af2925703269dba734d12be3f05
Artyom Gavrilov <artyom.e.gavrilov@gmail.com>
2026-01-08 17:47:11 +0300

    fix

--- template.sh
+++ template.sh
@@ -6,6 +6,7 @@ OUTPUT_FILE=""
 
 PRE=""
 RUN=""
+BENCH="perf stat -C $CPU -x, -e duration_time -- taskset -c $CPU $RUN"
 
 times=()
 
@@ -18,18 +19,18 @@ for ((i=1; i<=ITERATIONS; i++)); do
 
     sync; echo 3 | sudo tee /proc/sys/vm/drop_caches > /dev/null
     
-    result=$(perf stat -C "$CPU" -x, -e duration_time -- taskset -c "$CPU" $RUN 2>&1)
+    result=$($BENCH 2>&1)
     
     exec_time=$(echo "$result" | tail -n 1 | cut -d, -f1)
     exec_time=$(printf "%010d" "$exec_time" | sed -E 's/(.*)(.{9})/\1.\2/; s/0+$//; s/\.$//')   
 
     times+=("$exec_time")
 
-    echo "Iteration $i/$ITERATIONS: $exec_time ns"
+    echo "Iteration $i/$ITERATIONS: $exec_time s"
 done
 
 echo "PRE=$PRE" > "$OUTPUT_FILE"
-echo "RUN=$RUN" > "$OUTPUT_FILE"
-echo "${times[@]}" > "$OUTPUT_FILE"
+echo "RUN=$RUN" >> "$OUTPUT_FILE"
+echo "${times[@]}" >> "$OUTPUT_FILE"
 
 echo "Done. Commands and times saved to $OUTPUT_FILE"
